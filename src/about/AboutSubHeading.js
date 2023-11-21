1c7ca12bb8f2b19df04cf2fb48f59a47f0ecb91f
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-11-21 20:43:26 +0530

    Working with Content

---
+++ src/about/AboutSubHeading.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+import "../styles/aboutMenu.css";
+
+const AboutSubHeading = ({ title, content, active, onClick, menuItem }) => {
+  
+  
+  const subContainerClass = `sub-container-${menuItem}`
+  return (
+    <div className={classNames(subContainerClass, { "active-subheading" : active })} >
+      <h3 onClick={onclick}> {title}</h3>
+      <div className="p-container">{content}</div>
+    </div>
+  );
+};
+export default AboutSubHeading;
