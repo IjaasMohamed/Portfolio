21419064ab6fdd6c65238dbf1e6fd5e4bee3daf9
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-11-21 17:21:02 +0530

    Update Background
    Successfully updated the backround

--- src/background/Background.js
+++ src/background/Background.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "../styles/background.css";
 import video from "../assets/bg-universe.mp4";
-import fallbackImg from "../assets/fallback-image.png";
+import fallbackImage from "../assets/fallback-image.png";
 
 const Background = () => {
   return (
     <>
       <div className="shadow-overlay"></div>
       <video
-        playInline
-        autopPlay
+        playsInline
+        autoPlay
         muted
         loop
         preload="auto"
         id="bg"
-        poster={fallbackImg}
+        poster={fallbackImage}
       >
-        <source src={video} type="video?mp4"></source>
+        <source src={video} type="video/mp4" />
       </video>
     </>
   );
