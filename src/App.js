e5b2eb12f90b60c41d1a370e884c40ad57ef9b8b
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-11-21 17:34:44 +0530

    player stats

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ import Projects from "./projects/Projects";
 import Contact from "./contact/Contact";
 import "./styles/app.css";
 import Background from "./background/Background.js";
-// import PlayerStats from "./playerStats/PlayerStats.js";
+import PlayerStats from "./playerStats/PlayerStats.js";
 
 const App = () => {
   return (
@@ -45,7 +45,7 @@ const App = () => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      {/* <PlayerStats /> */}
+      <PlayerStats />
     </Router>
   );
 };
---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const PlayerStats = () => {
+  return (
+    <div id="playerStats">
+      <h1>IJAAS FOUZER LEVEL 41</h1>
+      <div id="playerStats__lines">
+        <span id="player__lines__thick"></span>
+        <span id="player__lines_thin"></span>
+      </div>
+      <h2>Fullstack Developer</h2>
+    </div>
+  );
+};
+
+export default PlayerStats;
