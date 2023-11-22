4aa70ab251b1c4df827fb5c13aabfc0edac4f0e9
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-11-22 14:58:21 +0530

    projects
    Completed the projects

---
+++ src/projects/projectsData.js
@@ -0,0 +1,52 @@
+import projectOne from "../assets/project-1.png";
+import projectTwo from "../assets/project-2.png";
+import projectThree from "../assets/project-3.png";
+
+const projects = {
+  1: {
+    title: "some platformn",
+    image: projectOne,
+    description: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet. consecutur adipising elit, sed do giumsed
+          temopor incident ut labre et dolore magna aliqua.
+        </p>
+      </>
+    ),
+    github: "https://this libk shpould be added",
+    Demo: "https://this netify link",
+  },
+
+  2: {
+    title: "productivity app",
+    image: projectTwo,
+    description: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet. consecutur adipising elit, sed do giumsed
+          temopor incident ut labre et dolore magna aliqua.
+        </p>
+      </>
+    ),
+    github: "https://this libk shpould be added",
+    Demo: "https://this netify link",
+  },
+
+  3: {
+    title: "hotel management",
+    image: projectThree,
+    description: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet. consecutur adipising elit, sed do giumsed
+          temopor incident ut labre et dolore magna aliqua.
+        </p>
+      </>
+    ),
+    github: "https://this libk shpould be added",
+    Demo: "https://this netify link",
+  },
+};
+
+export default projects;
