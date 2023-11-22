edd7f6b80df15111d4826f02f94400fca2973c2f
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-11-22 10:37:16 +0530

    working with skills menu

---
+++ src/skills/skillsData.js
@@ -0,0 +1,22 @@
+const skills = {
+  1: [
+    { title: "HTML", level: 6 },
+    { title: "CSS", level: 5 },
+    { title: "HTML", level: 2 },
+    { title: "HTML", level: 3 },
+    { title: "Angular", level: 5 },
+    { title: "React", level: 4 },
+    { title: "Javascript", level: 4 },
+    { title: "Typescript", level: 6 },
+  ],
+  1: [
+    { title: "Java", level: 6 },
+    { title: "Sprinboot", level: 5 },
+    { title: "Python", level: 2 },
+    { title: "Mysql", level: 3 },
+    { title: "MongoDB", level: 5 },
+    { title: "NodeJs", level: 4 },
+    { title: "GIT", level: 4 },
+    { title: "ExpressJs", level: 6 },
+  ],
+};
